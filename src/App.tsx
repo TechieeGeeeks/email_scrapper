93f62a3229ba7ee5f4674cf0e076b9b0428e3523
Swayam <swayamkarle_ce_2020@ltce.in>
2023-11-05 00:04:41 +0530

    Email Scrapper working fine

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { request } from "http";
 import React, { useState, useEffect } from "react";
 
 function App() {
   const [activeTab, setActiveTab] = useState(0);
   const [scrapedEmails, setScrapedEmails] = useState<string[]>([]);
 
-  // Handler which listens to this events coming from webpage 
-  chrome.runtime.onMessage.addListener((request,sender,sendResponse)=>{
-    // Get Emails
+  // This handler listens to events coming from the webpage and updates the extension's state
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // Get emails from the webpage
     let emails = request.emails;
     // Update the state with the scraped emails
     setScrapedEmails(emails);
-  })
+  });
 
+  // This function retrieves information about the active tab in the browser window
   const getActiveTabInfo = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0) {
@@ -24,13 +24,15 @@ function App() {
     });
   }
 
-  // So this function reads from web Page and send data to my extension script
+  // This function reads data from the webpage and sends it to the extension script
   const scrapEmailDataFromPage = () => {
     const emailRegEx = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/gim;
     const emails = document.body.innerHTML.match(emailRegEx) || [];
-    chrome.runtime.sendMessage({emails});
+    // Send the scraped emails to the extension
+    chrome.runtime.sendMessage({ emails });
   } 
 
+  // This function triggers the scraping process when the "Scrap" button is clicked
   const scrapData = () => {
     if (activeTab !== 0) {
       chrome.scripting.executeScript({

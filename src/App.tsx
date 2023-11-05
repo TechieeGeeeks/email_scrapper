742fecf46f4a32558a7569c73211030d46012c8a
Swayam <swayamkarle_ce_2020@ltce.in>
2023-11-05 20:38:07 +0530

    Signle Contract can be Scrapped

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [activeTab, setActiveTab] = useState(0);
-  const [scrapedEmails, setScrapedEmails] = useState<string[]>([]);
+  const [scrapedContractCode, setScrapedContractCode] = useState<string>("");
 
   // This handler listens to events coming from the webpage and updates the extension's state
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // Get emails from the webpage
-    let emails = request.emails;
-    // Update the state with the scraped emails
-    setScrapedEmails(emails);
+    // Get the contract code from the webpage
+    let contractCode = request.contractCode;
+    // Update the state with the scraped contract code
+    setScrapedContractCode(contractCode);
   });
 
   // This function retrieves information about the active tab in the browser window
@@ -24,20 +24,27 @@ function App() {
     });
   }
 
-  // This function reads data from the webpage and sends it to the extension script
-  const scrapEmailDataFromPage = () => {
-    const emailRegEx = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/gim;
-    const emails = document.body.innerHTML.match(emailRegEx) || [];
-    // Send the scraped emails to the extension
-    chrome.runtime.sendMessage({ emails });
-  } 
+  // This function reads the contract code from the specified element and sends it to the extension script
+  const scrapContractCodeFromPage = () => {
+    // Select the specific element containing the contract code
+    const contractElement = document.querySelector("#editor1 > div.ace_scroller > div > div.ace_layer.ace_text-layer");
+
+    // Check if the element exists
+    if (contractElement) {
+      const contractCode = contractElement.textContent;
+      // Send the scraped contract code to the extension
+      chrome.runtime.sendMessage({ contractCode });
+    } else {
+      console.error("Contract element not found on the page.");
+    }
+  }
 
   // This function triggers the scraping process when the "Scrap" button is clicked
   const scrapData = () => {
     if (activeTab !== 0) {
       chrome.scripting.executeScript({
         target: { tabId: activeTab },
-        func: scrapEmailDataFromPage
+        func: scrapContractCodeFromPage
       }).then(() => console.log("Script injected"));
     }
   }
@@ -48,16 +55,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Email Scrapper</h1>
+      <h1>Contract Code Scraper</h1>
       <button onClick={scrapData}>Scrap</button>
 
       <div>
-        <h2>Scraped Emails</h2>
-        <ul>
-          {scrapedEmails.map((email, index) => (
-            <li key={index}>{email}</li>
-          ))}
-        </ul>
+        <h2>Scraped Contract Code</h2>
+        <pre>{scrapedContractCode}</pre>
       </div>
     </div>
   );
